9fa997484ed8b595e37eecb6ced38dd8adb75952
Lynabch <42008865@parisnanterre.fr>
2026-02-09 11:25:02 +0100

    création portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Affiche l'année actuelle dynamiquement dans le footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Vous pouvez ajouter ici le code pour le menu burger mobile
+console.log("Portfolio chargé avec succès !");
